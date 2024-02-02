a361b4ef3a9fd57c4a8a42facce03436174f5664
chandra sekhar <chandrasekharlucs@gmail.com>
2024-02-02 20:05:30 +0530

    import

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Spinner, calc } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
