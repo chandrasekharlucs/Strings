a66eb7cbe05fb690cd7e6c0856c5b50049341043
chandra sekhar <chandrasekharlucs@gmail.com>
2024-03-06 19:30:32 +0530

    added toast for images in login

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,9 @@
 import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
 import React from "react";
 import AuthForm from "../../components/AuthForm/AuthForm";
-
+import useShowToast from "../../hooks/useShowToast";
 export default function AuthPage() {
+  const showToast = useShowToast();
   return (
     <Flex
       minH={"100vh"}
@@ -21,8 +22,18 @@ export default function AuthPage() {
             <AuthForm />
             <Box textAlign={"center"}>Get the App.</Box>
             <Flex gap={5} justifyContent={"center"}>
-              <Image src="/playstore.png" h="10" alt="playstore logo" />
-              <Image src="/microsoft.png" h="10" alt="microsoft logo" />
+              <Image
+                src="/playstore.png"
+                h="10"
+                alt="playstore logo"
+                onClick={() => showToast("Error", "Coming soon", "error")}
+              />
+              <Image
+                src="/microsoft.png"
+                h="10"
+                alt="microsoft logo"
+                onClick={() => showToast("Error", "Coming soon", "error")}
+              />
             </Flex>
           </VStack>
         </Flex>
